59761b42f7b8038f4f263480e8f06a47e4602973
Steve <stefan.steinheber@googlemail.com>
2017-04-22 13:33:12 +0200

    Vektor-Klasse Hinzugefügt

---
+++ ProjektInfromatik/src/matrizen/core/Vektor.java
@@ -0,0 +1,64 @@
+package matrizen.core;
+
+/**
+* @author Steve
+*/
+public class Vektor {
+	private float x, y;
+	
+	public Vektor() {
+		this(0, 0);
+	}
+	
+	public Vektor(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Vektor add(float x, float y) {
+		return addieren(x, y);
+	}
+	
+	public Vektor addieren(float x, float y) {
+		this.x += x;
+		this.y += y;
+		return this;
+	}
+	
+	public Vektor sub(float x, float y) {
+		return subtrahieren(x, y);
+	}
+	
+	public Vektor subtrahieren(float x, float y) {
+		return addieren(-x, -y);
+	}
+	
+	public Vektor mult(float m) {
+		return multiplizieren(m);
+	}
+	
+	public Vektor multiplizieren(float m) {
+		this.x *= m;
+		this.y *= m;
+		return this;
+	}
+	
+	public Vektor div(float m) {
+		return dividieren(m);
+	}
+	
+	public Vektor dividieren(float m) {
+		this.x /= m;
+		this.y /= m;
+		return this;
+	}
+	
+	//TODO
+	
+	public float mag() {
+		return 0.0;
+	}
+	
+	
+	
+}
