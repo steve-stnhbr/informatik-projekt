6439e92e84535e82227141d55b3576a2c667cdd2
Stefan <Stefan@NB-STEFAN.fritz.box>
2017-04-29 13:46:30 +0200

    Getter und Setter hinzugefügt

--- ProjektInfromatik/src/matrizen/core/Vektor.java
+++ ProjektInfromatik/src/matrizen/core/Vektor.java
@@ -56,4 +56,21 @@ public class Vektor {
 	public float mag() {
 		return (float) Math.sqrt(x*x+y*y);
 	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+	
 }
