0189a7b53cd185b927918da2f8a2d495c911dbac
Stefan <Stefan@NB-STEFAN.fritz.box>
2017-04-30 16:08:54 +0200

    Klasse mit Konstruktor erstellt

---
+++ ProjektInfromatik/src/matrizen/gui/SpielFenster.java
@@ -0,0 +1,11 @@
+package matrizen.gui;
+
+import javax.swing.JFrame;
+
+public class SpielFenster extends JFrame {
+	private Spiel spiel;
+	
+	public SpielFenster(Spiel spiel) {
+		this.spiel = spiel;
+	}
+}
