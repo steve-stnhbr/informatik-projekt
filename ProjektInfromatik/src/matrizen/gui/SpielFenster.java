673890ef706dada4cf253eca90a22a4fa878ab2d
StSteinh <StSteinh@CR3-07.schule.local>
2017-05-02 14:29:12 +0200

    Singleton für Spiel und SpielFenster eingefügt

--- ProjektInfromatik/src/matrizen/gui/SpielFenster.java
+++ ProjektInfromatik/src/matrizen/gui/SpielFenster.java
@@ -3,5 +3,12 @@ package matrizen.gui;
 import javax.swing.JFrame;
 
 public class SpielFenster extends JFrame {
-
+	private static SpielFenster instanz;
+	
+	
+	public static SpielFenster gibInstanz() {
+		if(instanz == null)
+			instanz = new SpielFenster();
+		return instanz;
+	}
 }
--- ProjektInfromatik/src/matrizen/view/Spiel.java
+++ ProjektInfromatik/src/matrizen/view/Spiel.java
@@ -1,6 +1,15 @@
 package matrizen.view;
 
 public class Spiel {
+	private static Spiel instanz;
+	
+	
+	public static Spiel gibInstanz() {
+		if(instanz == null)
+			instanz = new Spiel();
+		return instanz;
+	}
+	
 	public static void main(String[] args){
 		
 	}
