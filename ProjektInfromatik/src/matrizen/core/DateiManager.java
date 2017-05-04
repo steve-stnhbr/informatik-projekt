5eb85be0f2b44d0ccfde5ff7d0b005c5f7ea5cb3
Stefan <Stefan@NB-STEFAN.fritz.box>
2017-05-04 15:10:01 +0200

    DateiManager geschrieben

--- ProjektInfromatik/src/matrizen/core/DateiManager.java
+++ ProjektInfromatik/src/matrizen/core/DateiManager.java
@@ -11,46 +11,55 @@ public class DateiManager {
 
 	
 	static class Grafik {
-		private static BufferedImage src;
+		private static BufferedImage srcFeld, srcElement, srcPartikel;
 		
 		public static Image laden(Bild b) {
-			return src.getSubimage(b.x, b.y, 32, 32);
+			return b.src.getSubimage(b.x, b.y, b.groesse, b.groesse);
 		}
 		
 		static {
 			try {
-				src = ImageIO.read(new File("res/feld_res"));
+				if(srcFeld == null)
+					srcFeld = ImageIO.read(new File("res/feld_res"));
+				if(srcElement == null)
+					srcElement = ImageIO.read(new File("res/element_res"));
+				if(srcPartikel == null)
+					srcPartikel = ImageIO.read(new File("res/partikel_res"));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		
 		enum Bild {
-			feldStein0(0, 0),
-			feldStein1(0, 32),
-			feldStein2(0, 64),
-			feldStein3(0, 96),
-			feldGras0(32, 0),
-			feldGras1(32, 32),
-			feldGras2(32, 64),
-			feldGras3(32, 96),
-			feldSteinchen0(64, 0),
-			feldSteinchen1(64, 32),
-			feldSteinchen2(64, 64),
-			feldSteinchen4(64, 96),
-			feldSchotter0(96, 0),
-			feldSchotter1(96, 32),
-			feldSchotter2(96, 64),
-			feldSchotter3(96, 96),
-			feldBaum0(128, 0),
-			feldBaum1(128, 32),
-			elementSpieler(160, 0);
+			feldStein0(0, 0, 32, srcFeld),
+			feldStein1(0, 32, 32, srcFeld),
+			feldStein2(0, 64, 32, srcFeld),
+			feldStein3(0, 96, 32, srcFeld),
+			feldGras0(32, 0, 32, srcFeld),
+			feldGras1(32, 32, 32, srcFeld),
+			feldGras2(32, 64, 32, srcFeld),
+			feldGras3(32, 96, 32, srcFeld),
+			feldSteinchen0(64, 0, 32, srcFeld),
+			feldSteinchen1(64, 32, 32, srcFeld),
+			feldSteinchen2(64, 64, 32, srcFeld),
+			feldSteinchen4(64, 96, 32, srcFeld),
+			feldSchotter0(96, 0, 32, srcFeld),
+			feldSchotter1(96, 32, 32, srcFeld),
+			feldSchotter2(96, 64, 32, srcFeld),
+			feldSchotter3(96, 96, 32, srcFeld),
+			feldBaum0(128, 0, 32, srcFeld),
+			feldBaum1(128, 32, 32, srcFeld),
+			elementSpieler(0, 0, 32, srcElement),
+			elementSchluessel(0, 32, 32, srcElement);
 			
-			public int x, y;
+			public int x, y, groesse;
+			public BufferedImage src;
 			
-			private Bild(int x, int y) {
+			private Bild(int x, int y, int sc, BufferedImage image) {
 				this.x = x;
 				this.y = y;
+				this.groesse = sc;
+				this.src = image;
 			}
 			
 			public Bild zufaelligerStein() {
