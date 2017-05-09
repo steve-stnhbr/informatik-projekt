c8b6944e34a8362a20b9c41ef65786bd2f16e548
Stefan <Stefan@NB-STEFAN.fritz.box>
2017-05-09 19:02:36 +0200

    DateiManager verbessert & Feld.Typ hinzugefügt

--- ProjektInfromatik/src/matrizen/model/Feld.java
+++ ProjektInfromatik/src/matrizen/model/Feld.java
@@ -1,12 +1,49 @@
 package matrizen.model;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import matrizen.core.DateiManager;
 
 public class Feld extends Grafisch {
 	private boolean solide;
 
+	public Feld(Typ t) {
+		this(t.gibGrafik(), t.solide);
+	}
+	
+	private Feld(BufferedImage grafik, boolean solide) {
+		super();
+		this.grafik = grafik;
+		this.solide = solide;
+	}
+	
 	@Override
 	public void zeichnen(Graphics2D g) {
 		
+	}
+	
+	public enum Typ {
+		WASSER(true),
+		WIESE(false),
+		BAUM(true),
+		STEIN(false),
+		STEINCHEN(true),
+		ERDE(false),
+		SCHOTTER(false);
+		
+		final boolean solide;
+		
+		private Typ(boolean solide) {
+			this.solide = solide;
+		}
+		
+		public BufferedImage gibGrafik() {
+			return DateiManager.laden(DateiManager.Bild.zufaelligeGrafik(this));
+		}
+		
+		public static Typ gibTyp(int i) {
+			return values()[i];
+		}
 	}
 }
