2bbf7e94212e2f940e37249f52990fae12bce99a
Stefan <Stefan@NB-STEFAN.fritz.box>
2017-05-12 15:51:52 +0200

    lol

--- ProjektInfromatik/src/matrizen/core/Konfiguration.java
+++ ProjektInfromatik/src/matrizen/core/Konfiguration.java
@@ -1,7 +1,22 @@
 package matrizen.core;
 
+import static java.awt.event.KeyEvent.*;
+
 public class Konfiguration {
+	public static final Konfiguration standardWASD = new Konfiguration(VK_W, VK_D, VK_S, VK_A, VK_SPACE),
+			standardPfeil = new Konfiguration(VK_UP, VK_RIGHT, VK_DOWN, VK_LEFT, VK_SPACE);
 	private int rechts, links, oben, unten, schuss;
+
+	public Konfiguration() {}
+
+	public Konfiguration(int oben, int rechts, int unten, int links, int schuss) {
+		super();
+		this.rechts = rechts;
+		this.links = links;
+		this.oben = oben;
+		this.unten = unten;
+		this.schuss = schuss;
+	}
 
 	public int getRechts() {
 		return rechts;
