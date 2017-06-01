88d040557e50da6c52c4b7bce6cd746d7905cc84
StSteinh <StSteinh@CR3-21.schule.local>
2017-06-01 10:23:00 +0200

    Zauberer hinzugefügt

--- ProjektInfromatik/src/matrizen/model/elemente/Gegner.java
+++ ProjektInfromatik/src/matrizen/model/elemente/Gegner.java
@@ -4,17 +4,7 @@ import java.awt.Graphics2D;
 
 import matrizen.core.Vektor;
 
-public class Gegner extends Figur {
+public abstract class Gegner extends Figur {
 
-	@Override
-	public void kraftAusueben(Vektor v) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void zeichnen(Graphics2D g) {
-		
-	}
-
+	public abstract void angriff();
 }
---
+++ ProjektInfromatik/src/matrizen/model/gegner/Zauberer.java
@@ -0,0 +1,5 @@
+package matrizen.model.gegner;
+
+public class Zauberer {
+
+}
